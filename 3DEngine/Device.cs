22ab0f57b6a7f69048ef3f6860856aee1516f140
Milan Singh <milanprsingh@gmail.com>
2017-03-31 14:41:53 -0400

    Added Mesh and Camera classes. Started Device class

---
+++ 3DEngine/Device.cs
@@ -0,0 +1,10 @@
+﻿using Windows.UI.Xaml.Media.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+using SharpDX;
+
+namespace _3DEngine
+{
+    public class Device //This class will be the core of our engine
+    {
+    }
+}
---
+++ 3DEngine/Mesh.cs
@@ -0,0 +1,18 @@
+﻿using SharpDX;
+
+namespace _3DEngine
+{
+    public class Mesh
+    {
+        public string Name { get; set; }
+        public Vector3[] Verts { get; private set; }
+        public Vector3 Pos { get; set; }
+        public Vector3 Rot { get; set; }
+
+        public Mesh(string name, int numVert)
+        {
+            Verts = new Vector3[numVert];
+            Name = name;
+        }
+    }
+}
